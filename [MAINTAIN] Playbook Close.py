3c9590cad21253d001b46685549e47853fcb3f71
admin <root@localhost>
2022-04-29 10:59:03 +0000

    Use CF to run playbooks

--- [MAINTAIN] Playbook Close.py
+++ [MAINTAIN] Playbook Close.py
@@ -52,127 +52,11 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
 
     # call connected blocks if condition 1 matched
     if found_match_1:
-        get_close_playbook(action=action, success=success, container=container, results=results, handle=handle)
+        run_playbook_3(action=action, success=success, container=container, results=results, handle=handle)
         return
 
     # check for 'else' condition 2
     custom_function_failed_comment(action=action, success=success, container=container, results=results, handle=handle)
-
-    return
-
-
-def get_close_playbook(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("get_close_playbook() called")
-
-    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
-
-    parameters = []
-
-    parameters.append({
-        "location": "playbook?_filter_name='[Quick Close] No Threat'",
-        "verify_certificate": False,
-    })
-
-    ################################################################################
-    ## Custom Code Start
-    ################################################################################
-
-    # Write your custom code here...
-
-    ################################################################################
-    ## Custom Code End
-    ################################################################################
-
-    phantom.act("get data", parameters=parameters, name="get_close_playbook", assets=["phantom_http"], callback=format_list_of_items_and_playbook_ids)
-
-    return
-
-
-def format_list_of_items_and_playbook_ids(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("format_list_of_items_and_playbook_ids() called")
-
-    get_custom_list_copy_1__result = phantom.collect2(container=container, datapath=["get_custom_list_copy_1:custom_function_result.data.list_of_items"])
-    get_close_playbook_result_data = phantom.collect2(container=container, datapath=["get_close_playbook:action_result.data.*.parsed_response_body.data.*.id"], action_results=results)
-
-    get_custom_list_copy_1_data_list_of_items = [item[0] for item in get_custom_list_copy_1__result]
-    get_close_playbook_result_item_0 = [item[0] for item in get_close_playbook_result_data]
-
-    format_list_of_items_and_playbook_ids__list_of_items = None
-    format_list_of_items_and_playbook_ids__list_of_playbook_id = None
-
-    ################################################################################
-    ## Custom Code Start
-    ################################################################################
-
-    # Write your custom code here...
-
-    ################################################################################
-    ## Custom Code End
-    ################################################################################
-
-    phantom.save_run_data(key="format_list_of_items_and_playbook_ids:list_of_items", value=json.dumps(format_list_of_items_and_playbook_ids__list_of_items))
-    phantom.save_run_data(key="format_list_of_items_and_playbook_ids:list_of_playbook_id", value=json.dumps(format_list_of_items_and_playbook_ids__list_of_playbook_id))
-
-    format_json_for_container(container=container)
-
-    return
-
-
-def format_json_for_container(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("format_json_for_container() called")
-
-    template = """%%\n{{\n    \"container_id\": {0},\n    \"playbook_id\": {1},\n    \"scope\": \"all\",\n    \"run\": true\n}}\n%%\n"""
-
-    # parameter list for template variable replacement
-    parameters = [
-        "format_list_of_items_and_playbook_ids:custom_function:list_of_items",
-        "format_list_of_items_and_playbook_ids:custom_function:list_of_playbook_id"
-    ]
-
-    ################################################################################
-    ## Custom Code Start
-    ################################################################################
-
-    # Write your custom code here...
-
-    ################################################################################
-    ## Custom Code End
-    ################################################################################
-
-    phantom.format(container=container, template=template, parameters=parameters, name="format_json_for_container")
-
-    run_playbook(container=container)
-
-    return
-
-
-def run_playbook(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("run_playbook() called")
-
-    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
-
-    formatted_data___value = container.get("formatted_data__", None)
-
-    parameters = []
-
-    if formatted_data___value is not None:
-        parameters.append({
-            "body": formatted_data___value,
-            "location": "/playbook_run",
-            "verify_certificate": False,
-        })
-
-    ################################################################################
-    ## Custom Code Start
-    ################################################################################
-
-    # Write your custom code here...
-
-    ################################################################################
-    ## Custom Code End
-    ################################################################################
-
-    phantom.act("post data", parameters=parameters, name="run_playbook", assets=["phantom_http"])
 
     return
 
@@ -191,6 +75,35 @@ def custom_function_failed_comment(action=None, success=None, container=None, re
     ################################################################################
 
     phantom.comment(container=container, comment="The retrieve custom list data custom function has failed.")
+
+    return
+
+
+def run_playbook_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("run_playbook_3() called")
+
+    get_custom_list_copy_1__result = phantom.collect2(container=container, datapath=["get_custom_list_copy_1:custom_function_result.data.list_of_items"])
+
+    get_custom_list_copy_1_data_list_of_items = [item[0] for item in get_custom_list_copy_1__result]
+
+    parameters = []
+
+    parameters.append({
+        "container_list": get_custom_list_copy_1_data_list_of_items,
+        "playbook_name": "[Quick Close] No Threat",
+    })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="myPhantom/run_playbook", parameters=parameters, name="run_playbook_3")
 
     return
 
