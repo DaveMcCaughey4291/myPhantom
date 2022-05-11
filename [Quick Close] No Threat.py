9a7f4210d17d0f92029a44c6883adeee02df1a93
admin <root@localhost>
2022-05-11 11:26:47 +0000

    Removing tag block for testing

--- [Quick Close] No Threat.py
+++ [Quick Close] No Threat.py
@@ -34,34 +34,6 @@ def add_comment_set_status_1(action=None, success=None, container=None, results=
 
     container = phantom.get_container(container.get('id', None))
 
-    power_add_tag_2(container=container)
-
-    return
-
-
-def power_add_tag_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("power_add_tag_2() called")
-
-    parameters = []
-
-    parameters.append({
-        "tag_list": "[\"linux\",\"windows\"]",
-        "container_id": None,
-        "container_list": None,
-    })
-
-    ################################################################################
-    ## Custom Code Start
-    ################################################################################
-
-    # Write your custom code here...
-
-    ################################################################################
-    ## Custom Code End
-    ################################################################################
-
-    phantom.custom_function(custom_function="myPhantom/power_add_tag", parameters=parameters, name="power_add_tag_2")
-
     return
 
 
